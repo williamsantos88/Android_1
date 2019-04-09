71f1a6d48dc47255b570436420b73e8979de1b13
William Santos <william.santos88@gmail.com>
2019-04-09 20:47:56 -0300

    Signed-off-by: William Santos <william.santos88@gmail.com> Criando uma lista de alunos com LinearLayout

--- app/src/main/java/br/com/myapplication/ListaAlunoActivity.java
+++ app/src/main/java/br/com/myapplication/ListaAlunoActivity.java
@@ -2,12 +2,20 @@ package br.com.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ListaAlunoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_lista_alunos);
+
+        String[] alunos = {"William Santos", "Elizane de Oliveira", "Ana lúcia Santos", "Tamires Santos"};
+        ListView alunosList = (ListView) findViewById(R.id.alunosList);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos);
+        alunosList.setAdapter(adapter);
     }
 }
