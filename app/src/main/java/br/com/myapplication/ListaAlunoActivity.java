d66a734c3b86eb694224d21a4f87ba3e956ba1d9
William Santos <william.santos88@gmail.com>
2019-04-09 13:36:41 -0300

    Signed-off-by: William Santos <william.santos88@gmail.com> Criação do projeto Agenda do curso

---
+++ app/src/main/java/br/com/myapplication/ListaAlunoActivity.java
@@ -0,0 +1,13 @@
+package br.com.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListaAlunoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
